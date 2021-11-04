bc20cc17c9afae460cd2a904c385c28308ab24bc
Marco Levrero <marco.levrero@gmail.com>
2021-11-04 20:41:58 +0100

    javascript

--- app/Main.hs
+++ app/Main.hs
@@ -229,3 +229,57 @@ printAtom = \case
 
 indent :: Int -> String -> String
 indent tabs s = concat (replicate tabs "  ") ++ s
+
+-------
+-- JavaScript
+-------
+
+data JSExpr
+  = JSInt Int -- ATOM Int
+  | JSSymbol Name -- ATOM Symbol
+  | JSBinOp JSBinOp JSExpr JSExpr -- binary expr (*, +, etc.)
+  | JSLambda [Name] JSExpr -- anonymous functions
+  | JSFunCall JSExpr [JSExpr] -- function calls
+  | JSReturn JSExpr -- return values from functions
+  deriving (Eq, Show, Read)
+
+type JSBinOp = String
+
+-------
+-- Codegen
+-------
+
+printJSExpr :: Bool -> Int -> JSExpr -> String
+printJSExpr doIndent tabs = \case
+  JSInt i -> show i
+  JSSymbol name -> name
+  JSBinOp op e1 e2 ->
+    parensConcat
+      [ printNoIndent e1,
+        " ",
+        printJSOp op,
+        " ",
+        printNoIndent e2
+      ]
+  JSLambda vars expr ->
+    indentIf $
+      unlines
+        [ "function(" ++ intercalate ", " vars ++ ") {",
+          indent (tabs + 1) $ printJSExpr False (tabs + 1) expr
+        ]
+        ++ indent tabs "}"
+  JSFunCall f exprs ->
+    parensConcat
+      [ printNoIndent f,
+        ")(",
+        intercalate ", " (printNoIndent <$> exprs)
+      ]
+  JSReturn expr -> indentIf $ "return " ++ printNoIndent expr ++ ";"
+  where
+    indentIf = if doIndent then indent tabs else id
+    printNoIndent = printJSExpr False tabs
+    parensConcat = parens . mconcat
+    parens = \s -> "(" ++ s ++ ")"
+
+printJSOp :: JSBinOp -> String
+printJSOp op = op
